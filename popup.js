30b24cbd72f17459d0637dd5ba13b78c591095a0
Vincent Fagioli <vincent.fagioli@cpnv.ch>
2018-05-18 09:27:05 +0200

    Ajout de commentaire
    Ajout de commentaire

--- popup.js
+++ popup.js
@@ -1,27 +1,23 @@
 changeFont.onclick = function() {
+    //Obtient l'onglet sur lequel le plugin est lancé
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
+      //Injecte du Javascript sur l'onglet précédemment récupérer
+        chrome.tabs.executeScript(
           tabs[0].id,
           {code: 'alert("Je ne suis pas encore prêt!!");'});
     });
-  };
+};
 changeMale.onclick = function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-   
-              chrome.tabs.executeScript(
+        chrome.tabs.executeScript(
           tabs[0].id,
           {code: 'alert("Je ne suis pas encore prêt!!");'});
-        
-        
     });
-  };
+};
 changeFemale.onclick = function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-   
-              chrome.tabs.executeScript(
+        chrome.tabs.executeScript(
           tabs[0].id,
           {code: 'alert("Je ne suis pas encore prêt!!");'});
-        
-        
     });
-  };
+};
