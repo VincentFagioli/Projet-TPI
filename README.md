7b378f409e3a2b4f10aa7592726b0712264b6a71
Vincent Fagioli <vincent.fagioli@cpnv.ch>
2018-05-22 16:30:46 +0200

    Test changement grammatical masculin
    Après plusieurs tests avec la console de Google Chrome, test du code permettant de passer des phrases non épicènes à des phrases de genre masculin directement avec les boutons se trouvant sur la page html

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Installation:
 1: Sur votre ordinateur, ouvrez Chrome.
 2: Dans l'angle supérieur droit, cliquez sur Plus puis Plus d'outils puis Extensions.
 3: Dans l'angle supérieur droit, activez le mode développeur.
-4: Cliquez sur chargez l'extension non empaquetée et séléctionner le dossier ou ce trouve le projet.
+4: Cliquez sur chargez l'extension non empaquetée et sélectionner le dossier ou ce trouve le projet.
 
 Installation:
 
--- test.js
+++ test.js
@@ -5,3 +5,19 @@ testFont.onclick = function() {
         span[i].style.fontFamily = 'opendyslexic';
     }
 }
+
+testGenderMale.onclick = function(){
+    var regex = /(\·\w*)/g;
+    var rer = "";
+    var span = document.getElementsByClassName('Grammaticaltype');
+    var i;
+    for (i = 0; i < span.length; i++) {
+        var chaine =  span[i].innerText;
+        var resultat = chaine.replace(regex, rer);
+        console.log(resultat);
+        span[i].innerText = "ff";
+    }
+}
+
+
+
