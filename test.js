6bfa496ff98d4268177305291131e8a5756d7672
Vincent Fagioli <vincent.fagioli@cpnv.ch>
2018-05-24 14:07:25 +0200

    Changement grammatical féminin
    Ajout du code précédemment tester dans d.js afin de passer de phrases non épicène à une lecture au féminin, le code interagit maintenant avec tous les objets ayant la bonne class

--- test.js
+++ test.js
@@ -18,6 +18,36 @@ testGenderMale.onclick = function(){
         span[i].innerText = resultat;
     }
 }
-
-
+testGenderFemale.onclick = function () {
+    var regex = /(\S*)+(?:\·)+(\S*)/mg;
+    var regex2 = /\S*\·+\S*/m;
+    var span = document.getElementsByClassName('Grammaticaltype');
+    var m;
+    var resultat;
+    var i;
+    for (i = 0; i < span.length; i++) {
+        var str = span[i].innerText;
+        var temp=str;
+        while ((m = regex.exec(str)) !== null) {
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+            m.forEach((match, groupIndex) => {
+                if (`${groupIndex}` == 2) {
+                    if (m[groupIndex] === "e") {
+                        resultat = m[1] + m[groupIndex];
+                    }
+                    else if (m[groupIndex] === "es") {
+                        resultat = m[1].replace(/s+$/g, m[groupIndex]);
+                    }
+                    else {
+                        resultat = m[1].substring(0, m[1].lastIndexOf(m[groupIndex].charAt(0))) + m[groupIndex]; //début à caractère trouver
+                    }
+                }
+            });
+            temp = temp.replace(regex2, resultat);
+        }
+        span[i].innerText = temp;
+    }
+}
 
